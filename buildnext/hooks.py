8066cf35e20f18348b3d80179df2ba8a0b4dcfb2
akhila <akhilaraveeendran@gmail.com>
2026-07-06 12:08:25 +0000

    feat: add required apps

--- buildnext/hooks.py
+++ buildnext/hooks.py
@@ -8,7 +8,7 @@ app_license = "mit"
 # Apps
 # ------------------
 
-# required_apps = ["india_compliance"]
+required_apps = ["erpnext", "india_compliance"]
 
 # Each item in the list will be shown as an app in the apps page
 # add_to_apps_screen = [
